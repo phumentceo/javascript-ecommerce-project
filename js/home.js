71c5b2e42172c0a203859e164ef426c9366fe6fd
phumentceo <roungchanrith8@gmail.com>
2025-02-04 16:12:51 +0700

    Slide

--- js/home.js
+++ js/home.js
@@ -29,10 +29,29 @@ const categories = [
     }
 ];
 
+const slides = [
+    {
+        "id"    : 1,
+        "name"  : "Phone",
+        "image" : "https://media.product.which.co.uk/prod/images/original/f4e8c0ef7c7b-phones-vertical-header.jpg"
+    },
+    {
+        "id"   : 2,
+        "name" : "Computer",
+        "image": "https://allgen.com/wp-content/uploads/2019/11/san-antonio-computer-store-parts-department.jpg"
+    },
+    {
+        "id"   : 3,
+        "name" : "Watch",
+        "image": "https://rotarywatches.com/cdn/shop/files/Rotary_seamatic_yellow_dial_desktop_banner.png?v=1729177044&width=3200"
+    }
+]
+
 
 
 
 var categoryShow = document.querySelector(".categories-block");
+
 
 categories.map((item,index) => {
     
@@ -64,4 +83,19 @@ categories.map((item,index) => {
 
         categoryShow.innerHTML += menu;
     }
+})
+
+
+//inner slides to html
+var showAllSlide = document.querySelector(".all-slides");
+
+slides.map((item,index) => {
+    let slideHtml = ``;
+    slideHtml += `
+        <div class="carousel-item ${index == 0 ? 'active' : ''}" data-bs-interval="5000">
+            <img src="${item.image}" class="d-block w-100" alt="...">
+        </div>
+    `;
+
+    showAllSlide.innerHTML += slideHtml;
 })
