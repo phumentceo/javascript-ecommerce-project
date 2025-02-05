1500578d8d41367002e4433783ec3a51b9cb5c5d
phumentceo <roungchanrith8@gmail.com>
2025-02-05 16:49:34 +0700

    Category

--- js/home.js
+++ js/home.js
@@ -3,18 +3,21 @@ const categories = [
         "id" : 1,
         "name" : "Phone",
         "stock" : 50,
+        "image" : "https://cdn.mos.cms.futurecdn.net/AVEcca7TuDmt8wjaFZPkzj.jpg",
         "description" : "Phone new stock"
     },
     {
        "id" : 2,
        "name" : "Computer",
        "stock" : 50,
+       "image" : "https://thumbs.dreamstime.com/z/variety-computers-both-laptop-desktops-sale-computer-store-computers-sale-computer-store-135429635.jpg",
        "description" : "Computer new stock" 
     },
     {
         "id" : 3,
         "name" : "Watch",
         "stock" : 20,
+        "image" : "https://images.squarespace-cdn.com/content/v1/55e87a4ae4b0ef4233ddf7a9/1444399319901-ND4DBMDC9FZTIHHDT6O0/IMG_0297.JPG",
         "types" :[
             {
                 "id" : 1,
@@ -63,10 +66,16 @@ categories.map((item,index) => {
             <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                ${item.name}
             </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Men</a></li>
-                <li><a class="dropdown-item" href="#">Woment</a></li>
-            </ul>
+            <ul class="dropdown-menu">`;
+
+            item.types.map((value) => {
+                dropdownMenu += `<li><a class="dropdown-item" href="#">${value.name}</a></li>`;
+            })
+
+                // <li><a class="dropdown-item" href="#">Men</a></li>
+                // <li><a class="dropdown-item" href="#">Woment</a></li>
+
+           dropdownMenu += `</ul>
         </li>
         `;
 
@@ -86,16 +95,60 @@ categories.map((item,index) => {
 })
 
 
-//inner slides to html
-var showAllSlide = document.querySelector(".all-slides");
+var showSlides = document.querySelector(".carousel-inner");
 
-slides.map((item,index) => {
+slides.map((value,index) => {
     let slideHtml = ``;
+
     slideHtml += `
-        <div class="carousel-item ${index == 0 ? 'active' : ''}" data-bs-interval="${index == 0 ? '5000' : '3000'}">
-            <img src="${item.image}" class="d-block w-100" alt="...">
-        </div>
+    <div class="carousel-item ${index == 0 ? 'active' : ''}" data-bs-interval="${index == 0 ? '5000' : '3000' }">
+        <img src="${value.image}" class="d-block w-100" alt="...">
+    </div>
     `;
 
-    showAllSlide.innerHTML += slideHtml;
-})
+    showSlides.innerHTML += slideHtml;
+});
+
+
+var showCategoryLeft  = document.querySelector(".category-left");
+var showCategoryRight = document.querySelector(".category-right");
+
+
+categories.map((value,index) => {
+    if(index !=2){
+        let leftInner = ``;
+        leftInner += `
+            <div class="col-12 category-data mb-2 ps-lg-0">
+                <div class="card">
+                    <img src="${value.image}" alt="">
+                    <div class="description">
+                        <h2>${value.name}</h2>
+                        <p>${value.description}</p>
+                        <p>${value.stock} product</p>
+                    </div>
+                    <div class="opacity"></div>
+                </div>
+            </div>
+        `;
+
+        //inner to html
+        showCategoryLeft.innerHTML += leftInner;
+    }else{
+        let rightInner = ``;
+
+        rightInner += `
+        <div class="card">
+            <img class="cate-watch" src="${value.image}" alt="">
+            <div class="description">
+                <h2>${value.name}</h2>
+                <p>${value.description}</p>
+                <p>${value.stock} product</p>
+            </div>
+            <div class="opacity-watch"></div>
+        </div>
+        `;
+
+        //inner to html
+        showCategoryRight.innerHTML += rightInner;
+    }
+});
